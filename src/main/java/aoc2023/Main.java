26b66bde5e9ea4e94c090b4d293ba04fc0c335d4
Jordi Poch <jpochc@gmail.com>
2023-12-05 07:54:35 +0100

    Create project structure

---
+++ src/main/java/aoc2023/Main.java
@@ -0,0 +1,7 @@
+package aoc2023;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/aoc2023/MainTest.java
@@ -0,0 +1,13 @@
+package aoc2023;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        Main.main();
+    }
+}
